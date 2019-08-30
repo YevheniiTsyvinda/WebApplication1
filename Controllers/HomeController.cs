d5e9845b016caf01e78fbca61bc3b5971efb5210
YevheniiTsyvinda <YevheniiTsyvinda@github.com>
2019-08-30 13:08:29 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using TinyCsvParser;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static string pathToData = "App_data/2017 01 AU3 Multichem.csv"; //"../../C:\Users\Professional\source\repos\WebApplication1\App_data/../App_data/2017 01 AU3 Multichem.csv";
+        public IActionResult Index()
+        {
+            CsvParserOptions csvParserOptions = new CsvParserOptions(true, ';');
+            ModelClassMap maper = new ModelClassMap();
+            CsvParser<Model> csvParser = new CsvParser<Model>(csvParserOptions, maper);
+            var result = csvParser.ReadFromFile(pathToData, Encoding.ASCII).ToList();
+           
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MyModel.cs
@@ -0,0 +1,59 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TinyCsvParser.Mapping;
+
+namespace WebApplication1.Models
+{
+    public class Model
+    {
+        public string Protocolname { get; set; }
+        public string Instrument { get; set; }
+        public string Parameter { get; set; }
+        public int Level { get; set; }
+        public DateTime Date { get; set; }
+        public string Value { get; set; }
+        public double Target { get; set; }
+        public double SD { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string Usercomment { get; set; }
+        public string User { get; set; }
+        public string SampleID { get; set; }
+    }
+
+    public class ModelClassMap : CsvMapping<Model>
+    {
+        public ModelClassMap():base()
+        {
+            MapProperty(0, m => m.Protocolname);
+            MapProperty(1, m => m.Instrument);
+            MapProperty(2, m => m.Parameter);
+            MapProperty(3, m => m.Level);
+            MapProperty(4, m => m.Date);//// error
+            MapProperty(5, m => m.Value);
+            MapProperty(6, m => m.Target);
+            MapProperty(7, m => m.SD);
+            MapProperty(8, m => m.Status);
+            MapProperty(9, m => m.Message);
+            MapProperty(10, m => m.Usercomment);
+            MapProperty(11, m => m.User);
+            MapProperty(12, m => m.SampleID);
+        }
+        public DateTime ConvertDate(DateTime date)
+        {
+            var d = DateTime.ParseExact(
+            date.ToString(), "ddd MMM dd yyyy HH:mm:ss 'GMT'K '(GMT Standard Time)'",
+            CultureInfo.InvariantCulture);
+            return d;
+        }
+       
+    }
+
+   
+}
